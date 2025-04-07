2f1c5fc38b8211d20c3989187c0d30dec0b5e166
FBmarko <turanmehmet2702@gmail.com>
2025-04-08 01:46:53 +0300

    312321321321

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
 def say_hello():
-    return "Merhaba Dunya! Ben bir Python dosyasından geldim!"
+    print("Merhaba Dunya! Ben ")
 
 if __name__ == "__main__":
-    # Uygulama 0.0.0.0 IP'sine bağlanmalı ki Docker'da dışarıdan erişilebilsin
-    app.run(host='0.0.0.0', port=5000)
+    say_hello()
